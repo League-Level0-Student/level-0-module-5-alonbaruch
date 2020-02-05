d48b68b748d182e261cc008d05ab29d437fa2b09
League Student <league@WTS14-3.attlocal.net>
2020-02-04 19:52:06 -0800

    Create Boi.java
    ewty

---
+++ src/_01_prime_or_not/Boi.java
@@ -0,0 +1,40 @@
+package _01_prime_or_not;
+
+import javax.swing.JOptionPane;
+
+public class Boi {
+public static void main(String[] args) {
+	String a = JOptionPane.showInputDialog(null, "choose a number.");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+}
