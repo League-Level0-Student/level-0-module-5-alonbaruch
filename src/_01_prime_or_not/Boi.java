dc5de9e780f11a5dd1f1d882bde4ba8608d338ad
League Student <league@WTS14-3.attlocal.net>
2020-02-11 19:56:36 -0800

    hi
    ckiken leg bis

--- src/_01_prime_or_not/Boi.java
+++ src/_01_prime_or_not/Boi.java
@@ -5,8 +5,17 @@ import javax.swing.JOptionPane;
 public class Boi {
 public static void main(String[] args) {
 	String a = JOptionPane.showInputDialog(null, "choose a number.");
+int b = Integer.parseInt(a);
+for (int i = 2; i < b; i++) {
+if(b%i==0) {
+	JOptionPane.showMessageDialog(null,"your number is not prime" );
+System.exit(0);
+}
+
+}
 
 
+	JOptionPane.showMessageDialog(null,"your number is prime" );
 
 
 
---
+++ src/_02_fibonacci/fibonacci.java
@@ -0,0 +1,20 @@
+package _02_fibonacci;
+
+public class fibonacci {
+	public static void main(String[] args) {
+		int a=0; int b=1; int c;
+	System.out.println(a);
+	System.out.println(b);
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	}
+
+}
